53092c66058b894957683f27f26781ddaf7e248a
Carolyn-Jiang <57229766+Carolyn-Jiang@users.noreply.github.com>
2020-02-28 01:12:05 -0500

    HW1 correction

--- FE595HW1.py
+++ FE595HW1.py
@@ -1,14 +1,13 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import style
 
 x = np.linspace(0,2*math.pi,1000)
 y = np.sin(x)
-plt.plot(x,y)
-plt.show()
-
-x0 = np.linspace(0,2*math.pi,1000)
 y0 = np.cos(x)
-plt.plot(x0,y0)
-plt.show()
+
+if __name__ == "__main__":
+	plt.plot(x,y)
+	plt.show()
+	plt.plot(x,y0)
+	plt.show()
